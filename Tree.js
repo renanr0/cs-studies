263f9945fa0c42021953317a0cf1a902e5ab8829
Renan Rodrigues <renanrodr0@gmail.com>
2024-04-11 21:56:20 +0200

    binary tree check

--- Tree.js
+++ Tree.js
@@ -34,15 +34,67 @@ class Node {
             }
         }
     }
+
+    printInOrder() {
+        if (this.left != null) {
+            this.left.printInOrder()
+        } 
+        console.log(this.data)
+        if (this.right != null) {
+            this.right.printInOrder()
+        }
+    }
+
+    printPreOrder() {
+        console.log(this.data)
+        if (this.left != null) {
+            this.left.printPreOrder()
+        } 
+        if (this.right != null) {
+            this.right.printPreOrder()
+        }
+    }
+
+    printPostOrder() {
+        if (this.left != null) {
+            this.left.printPreOrder()
+        } 
+        if (this.right != null) {
+            this.right.printPreOrder()
+        }
+        console.log(this.data)
+    }
+
+    isBST(min, max) {
+        if (this.data < min || this.data > max) {
+            return false 
+        }
+        
+        return (this.left == null || this.left.isBST(min, this.data)) && 
+        (this.right == null || this.right.isBST(this.data, max))
+    }
 }
 
-let root = new Node(3)
-root.insert(new Node(7))
-root.insert(new Node(6))
-root.insert(new Node(8))
-root.insert(new Node(3))
-root.insert(new Node(1))
+let root = new Node(50)
+root.insert(new Node(10))
+root.insert(new Node(5))
+root.insert(new Node(30))
+root.insert(new Node(20))
+root.insert(new Node(40))
+root.insert(new Node(80))
+root.insert(new Node(70))
+root.insert(new Node(90))
+root.insert(new Node(85))
 
-console.log(root.contains(8)) // true
-console.log(root.contains(10)) // false
-console.log(root.contains(6)) // true
+//root.printInOrder(root)
+// root.printPreOrder(root)
+
+console.log(root.isBST(-Infinity, Infinity))
+
+let rootNonBinaryTree = new Node(50)
+rootNonBinaryTree.left = new Node(10)
+rootNonBinaryTree.left.left = new Node(10)
+rootNonBinaryTree.left.right = new Node(30)
+rootNonBinaryTree.left.right.left = new Node(40) // forced wrong
+
+console.log(rootNonBinaryTree.isBST(-Infinity, Infinity))
